1a32a44efd71bdf135ee22a6b7c555fe5c7dcbf6
Richard DeWald <rdewald@gmail.com>
2022-02-05 14:02:11 -0500

    upload scripting

--- rdewald.R
+++ rdewald.R
@@ -1,5 +1,15 @@
 # oneDrive backup script
+# 
 library(Microsoft365R)
 od <- get_personal_onedrive()
-od$upload_file("staging/Bayesian_primer.pdf", dest = "Bayesian_primer.pdf")
-od$upload_file("staging/cost_of_hope.pdf", dest = "cost_of_hope.pdf")
+uploads <- list.files(Sys.getenv("ODMW_staging"))
+paste("staged for upload: ", uploads[])
+for (i in seq_along(uploads)) { 
+  od$upload_file(paste0(Sys.getenv("ODMW_staging")
+                        , "/"
+                        , stringr::str_trim(uploads[i]))
+                 , dest = paste0(Sys.getenv("ODMW_dest")
+                                 , "/"
+                                 , stringr::str_trim(uploads[i]))
+  )
+}
